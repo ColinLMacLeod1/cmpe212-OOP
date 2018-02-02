aee6a60d1db9c0e36b75904993ffaabe7165274b
Colin MacLeod <colinlmacleod1@gmail.com>
2018-02-02 18:34:15 -0500

    Finished the game of Pig

--- Assignment1/src/Assn1_14clm1.java
+++ Assignment1/src/Assn1_14clm1.java
@@ -4,17 +4,17 @@ import java.util.Random;
 public class Assn1_14clm1 {
 	public static Random generator = new Random(System.currentTimeMillis());
 	public static void main(String[] args) {
-		int humanScore = 0;
-		int compScore = 0;
-		int turns = 0;
-		System.out.println("Assignment1");		
-		while (humanScore<100 && compScore<100) {
+		int humanScore = 0; // The total player score at the beginning of each turn
+		int compScore = 0;  // The total computer score at the beginning of each turn
+		int turns = 0;      // Turn counter
+		System.out.println("Assignment1: The Game of Pig");		
+		while (humanScore<100 && compScore<100) { //Updating scores and counting turns
 			turns++;
-			humanScore += humanTurn();
-			compScore += compTurn();
-			System.out.println("Player: " + humanScore + ", Computer: " + compScore);
+			humanScore += humanTurn(humanScore);
+			compScore += compTurn(compScore);
+			System.out.println("Player's Sum is: " + humanScore + ", Computer's Sum is: " + compScore);
 		} // end while
-		if(humanScore>compScore) {
+		if(humanScore>compScore) {   // Defining victory conditions
 			System.out.println("The player wins in " + turns + " turns with " + humanScore + " points!");
 		} else if (humanScore<compScore){
 			System.out.println("The computer wins in "+ turns + " turns with " + compScore + " points!");
@@ -23,69 +23,78 @@ public class Assn1_14clm1 {
 		} // end if
 	} // end main()
 	
-	public static int humanTurn() {
-		System.out.println("Player's turn");
-		boolean noChoice = false;
-		boolean hold = UI(noChoice);	
-		int score = 0;
-		int[] roll;
+	public static int humanTurn(int totalScore) {
+		System.out.println("Player's turn:");
+		boolean noChoice = false;     // Indicator of whether or no the play will be force to roll again
+		boolean hold = UI(noChoice);  // Indicator of whether or not the turn will end
+		int score = 0;                // Turn score
+		int[] roll;                   // Array of the roll
 		
-		while (!hold) {			
+		while (!hold) {		          // While the player has not chosen to hold, roll	
 			noChoice = false;
 			roll = rollDice();
-			System.out.println("Roll: " + roll[0] + " " + roll[1]);
-			if(roll[0] == 1 && roll[1] == 1) {
+			System.out.println("Player rolled: " + num2String(roll[0]) + " " + num2String(roll[1]));
+			if(roll[0] == 1 && roll[1] == 1) {  // Setting different outcomes of a roll
 				score += 25;
 				noChoice = true;
 			} else if(roll[0] == 1 || roll[1] == 1) {
-				System.out.println("Human scored 0 points");
+				System.out.println("TURN OVER! Turn sum is zero!");
 				return 0;
+			} else if(roll[0] == roll[1]) {
+				score=+ 4*roll[0];
+				noChoice = true;
+				System.out.println("DOUBLES!");
 			} else {
 				score += roll[0] + roll[1];
-			}
-			System.out.println("Score on the line: " + score);
-			hold = UI(noChoice);
+			} // end if
+			System.out.println("Player's turn sum is: " + score + " and game sum would be: " + (score+ totalScore));
+			if(noChoice) System.out.println("Player must roll again!");
+			hold = UI(noChoice);    // Determining if the player must roll again
 		} // end while
 		return score;
 	} // end humanTurn()
 	
-	public static int compTurn() {
-		System.out.println("Computer's turn");
-		boolean noChoice = false;
-		boolean hold = false;	
-		int score = 0;
-		int[] roll;
-		int count = 0;
+	public static int compTurn(int totalScore) {
+		System.out.println("Computer's turn:");
+		boolean noChoice = false;  // Indicator of whether or no the play will be force to roll again
+		boolean hold = false;	   // Indicator of whether or not the turn will end
+		int score = 0;             // Turn score
+		int[] roll;                // Array of the roll
+		int count = 0;             // Roll count to limit the computer
 		
-		while (!hold) {			
+		while (!hold) {			   // While the player has not chosen to hold, roll	
 			noChoice = false;
 			roll = rollDice();
-			System.out.println(roll[0] + " " + roll[1]);
-			if(roll[0] == 1 && roll[1] == 1) {
+			System.out.println("Computer rolled:" + num2String(roll[0]) + " " + num2String(roll[1]));
+			if(roll[0] == 1 && roll[1] == 1) {  // Setting different outcomes of a roll
 				score += 25;
 				noChoice = true;
 			} else if(roll[0] == 1 || roll[1] == 1) {
-				System.out.println("Computer scored 0 points");
+				System.out.println("TURN OVER! Turn sum is zero!");
 				return 0;
+			} else if(roll[0] == roll[1]) {
+				score=+ 4*roll[0];
+				noChoice = true;
+				System.out.println("DOUBLES!");
 			} else {
 				score += roll[0] + roll[1];
-			}
+			} // end if
 			count++;
 			if(count > 2 && !noChoice) hold = true;
-			System.out.println("Score on the line: " + score);
+			System.out.println("Copmuter's turn sum is: " + score + " and game sum would be: " + (score+ totalScore));
+			if(noChoice) System.out.println("Copmuter must roll again!");
 		} // end while
 		return score;
 	} // end compTurn()
 	
-	public static int[] rollDice(){
-		
+	public static int[] rollDice(){	// Rolling the dice out putting an array of two integers
 		int[] roll = new int[2];
 		roll[0] = 1 + generator.nextInt(6);
 		roll[1] = 1 + generator.nextInt(6);
 		return roll;
 	} // end rollDice()
 	
-	public static boolean UI(boolean noChoice) {
+	public static boolean UI(boolean noChoice) {  // Taking the user input of either rolling or holding
 		String input;
 		boolean goodInput = false;
 		if(noChoice) return false;
@@ -94,12 +103,23 @@ public class Assn1_14clm1 {
 			if(input.equals("hold")) return true;
 			else if(input.equals("roll")) return false;
 			else {
-				JOptionPane.showMessageDialog(null, "Please enter 'hold' or 'roll'");
-				
-			}
-		}
+				JOptionPane.showMessageDialog(null, "Please enter 'hold' or 'roll'");			
+			} // end if
+		}// end while
 		return false;
-	}
+	} // end UI()
+	
+	public static String num2String(int num) {  // Converts ints to string for the roll
+		switch(num) {
+			case 1: return "one";
+			case 2: return "two";
+			case 3: return "three";
+			case 4: return "four";
+			case 5: return "five";
+			case 6: return "six";
+			default: return "error";
+		} // end switch 
+	} // end num2String()
 	
 	
 }
